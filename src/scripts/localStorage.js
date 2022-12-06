959e8b0f8f2651078883cbbb5c867396537c519b
Jean <jeanncavalcante@gmail.com>
2022-12-06 11:59:10 -0300

    adicionando funcao sair

--- src/scripts/localStorage.js
+++ src/scripts/localStorage.js
@@ -8,6 +8,8 @@ export const getDados = () =>
 export const setDados = (usuarios) =>
   localStorage.setItem("@usuarios", JSON.stringify(usuarios));
 
+  console.log(getUser());
+
 export const verificaToken = () => {
   const urlAtual = window.location.href.split(".html");
   const paginaCompleta = urlAtual[0].split("/");
@@ -18,16 +20,18 @@ export const verificaToken = () => {
     console.log("Inclui");
   } else {
     try {
+      console.log(getUser());
       const temToken = getUser()[0].token;
-      console.log(temToken);
+      console.log(!!temToken);
+
+      if (temToken == [] || temToken == null) {
+        window.location.href = "../../index.html";
+      }
     } catch (e) {
       window.location.href = "../../index.html";
     }
   }
 };
 
-export function sair() {
-  getUser() = [];
-}
 
-verificaToken();
+verificaToken()
